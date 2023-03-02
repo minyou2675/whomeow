c28f1351ac600e59aece2d392f7525c5aff3a3c4
minyou2675 <kymyy2675@gmail.com>
2023-03-02 12:33:21 +0900

    [feat] 메인화면 레이아웃 추가 v1

--- app/src/main/java/com/example/whomeow/MainActivity.kt
+++ app/src/main/java/com/example/whomeow/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.whomeow
 
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        val kakaoLoginBtn = findViewById<Button>(R.id.kakaoLoginBtn)
+
+//        val drawable = resources.getDrawable(R.drawable.kakao_login_medium_narrow)
+//
+//        kakaoLoginBtn.background = drawable
+//
+//        val radius = resources.getDimensionPixelSize(R.dimen.corner_radius).toFloat()
+//        val shape = GradientDrawable()
+//        shape.cornerRadius = radius
+//        kakaoLoginBtn.background = shape
     }
+
+
 }
