1bb1e3f8b9fce8908bce11ed3362dbb0d4729ce5
soo00h <cshyeon129@gmail.com>
2023-03-07 19:24:39 +0900

    first commit

---
+++ app/src/main/java/com/nycy1020/mainscreen/Findpassword3.kt
@@ -0,0 +1,11 @@
+package com.nycy1020.mainscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Findpassword3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_findpassword3)
+    }
+}
